489e23cfdb45fcdfe3b5ab83cafcd395fd5505ad
newtonah0 <ezmh015@gmail.com>
2023-11-20 20:44:54 +0300

    fix linter errors

--- menu_handler.rb
+++ menu_handler.rb
@@ -26,7 +26,7 @@ class MenuHandler
     puts '6. List all rentals for a person'
     puts '7. Quit'
   end
-  
+
   def handle_option(choice)
     option_method = @menu_options[choice] || :handle_invalid_option
     # Pass necessary arguments to create_person if the option is to create a person
@@ -36,29 +36,37 @@ class MenuHandler
       send(option_method)
     end
   end
+
   # Add the following methods
   def list_all_books
     @app.list_all_books
   end
+
   def list_all_people
     @app.list_all_people
   end
+
   def create_person
     @person_creator.create_person
   end
+
   def create_book
     @book_creator.create_book
   end
+
   def create_rental
     @rental_creator.create_rental
   end
+
   def list_rentals_for_person
     @rentals_lister.list_rentals_for_person
   end
+
   def quit
     puts 'Thanks for using the app, Goodbye!'
     exit
   end
+
   def handle_invalid_option
     puts 'Invalid option. Please try again.'
   end
