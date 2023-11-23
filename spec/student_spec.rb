0c791877eef8ec30d6012d21ea22d29be9b6cc82
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-23 13:36:08 +0300

    Test teacher class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,54 +1,23 @@
 require_relative 'spec_helper'
-require 'date'
-describe Person do
+describe Student do
+  let(:student_id) { 1 }
+  let(:student_age) { 20 }
+  let(:student_name) { 'John' }
+  let(:student) { Student.new(student_id, student_age, student_name) }
   describe '#initialize' do
-    it 'creates a new person object with the given attributes' do
-      person = Person.new(1, 18, 'John Doe', parent_permission: true)
-      expect(person.instance_variable_get(:@id)).to eq(1)
-      expect(person.instance_variable_get(:@age)).to eq(18)
-      expect(person.instance_variable_get(:@name)).to eq('John Doe')
-      expect(person.instance_variable_get(:@parent_permission)).to eq(true)
-      expect(person.instance_variable_get(:@rentals)).to eq([])
+    it 'creates a new Student instance with the given id, age, and name' do
+      expect(student.id).to eq(student_id)
+      expect(student.age).to eq(student_age)
+      expect(student.name).to eq(student_name)
     end
-    it 'sets the default name to "unknown" if not provided' do
-      person = Person.new(2, 16)
-      expect(person.instance_variable_get(:@name)).to eq('unknown')
+    it 'sets the name to "unknown" if not provided' do
+      student_without_name = Student.new(student_id, student_age)
+      expect(student_without_name.name).to eq('unknown')
     end
   end
-  describe '#can_use_services?' do
-    context 'when the person is of age' do
-      it 'returns true' do
-        person = Person.new(1, 20, 'Alice', parent_permission: false)
-        expect(person.can_use_services?).to eq(true)
-      end
-    end
-    context 'when the person is not of age but has parent permission' do
-      it 'returns true' do
-        person = Person.new(2, 16, 'Bob', parent_permission: true)
-        expect(person.can_use_services?).to eq(true)
-      end
-    end
-    context 'when the person is not of age and does not have parent permission' do
-      it 'returns false' do
-        person = Person.new(3, 15, 'Charlie', parent_permission: false)
-        expect(person.can_use_services?).to eq(false)
-      end
-    end
-  end
-  describe '#add_rental' do
-    it 'adds a rental to the person' do
-      person = Person.new(1, 18, 'David', parent_permission: true)
-      book = Book.new(1, 'The Great Gatsby', 'F. Scott Fitzgerald')
-      date = Date.new(2023, 11, 1)
-      expect { person.add_rental(date, book) }.to change { person.rentals.size }.by(2)
-    end
-  end
-  describe '#to_hash' do
-    it 'returns a hash with person attributes' do
-      person = Person.new(1, 25, 'Eve', parent_permission: true)
-      expected_hash = { 'id' => 1, 'age' => 25, 'name' => 'Eve' }
-      expect(person.to_hash).to eq(expected_hash)
+  describe '#play_hooky' do
+    it 'returns the play hooky message' do
+      expect(student.play_hooky).to eq('¯(ツ)/¯')
     end
   end
 end
-
